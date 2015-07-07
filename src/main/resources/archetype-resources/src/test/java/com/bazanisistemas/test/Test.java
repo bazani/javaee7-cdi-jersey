1c8cb5e0c1cd0ed8bbaebe4c3705f924b0e4abb1
Mateus Bazani <mateus@bazanisistemas.com>
2015-07-07 08:01:18 -0300

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/com/bazanisistemas/test/Test.java
@@ -0,0 +1,31 @@
+package com.bazanisistemas.test;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.NamingException;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class EntidadeTest {
+	@BeforeSuite
+	public void init() throws NamingException {
+		//starts openEJB container for tests
+		EJBContainer.createEJBContainer().getContext().bind("inject", this);
+	}
+	
+	@Test
+	public void shouldCreate() {
+		//Method contents
+	}
+	
+	@Test(dependsOnMethods = {"shouldCreate"}) 
+	public void shouldList() { 
+		//Method contents
+	}
+	
+	@Test(expectedExceptions = {javax.ejb.EJBException.class})
+	public void shouldFailPropertyRequired() {
+		//Method contents
+	}
+}
